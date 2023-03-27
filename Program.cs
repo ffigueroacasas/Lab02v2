7b32dfef5e980132163190c6ad89fb09ae1b9ce8
utnnet2019@outlook.com <utnnet2019@outlook.com>
2023-03-27 16:21:27 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab02v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor1 = "Este es el valor 1";
+            int valor2 = 5;
+            string valor3 = valor1;
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+            Console.ReadKey();
+        }
+    }
+}
