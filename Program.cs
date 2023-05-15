c69c874b2542ec03b68c09b3134fb044c6f9cd38
ffigueroacasas <93206842+ffigueroacasas@users.noreply.github.com>
2023-05-15 13:56:39 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,8 +12,6 @@ namespace Lab02v2
             Console.WriteLine(valor1);
             Console.WriteLine(valor2);
             Console.WriteLine(valor3);
-            Console.WriteLine("hhh");
-            Console.WriteLine("hola");
             Console.ReadKey();
         }
     }
